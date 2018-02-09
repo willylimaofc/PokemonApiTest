ca2afbfd21cf806b7dad861c37321cc1c52388bb
Leonardo Menezes <leonardomenezes@MacBook-Pro-de-Support-5.local>
2018-02-09 16:40:16 -0200

    Add mocha opts and Readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# PokemonApiTest
+
+Some 'poke-tests' in pokémon API, using mocha and chai.
+
+## Required to run this project
+
+- [Node](https://nodejs.org/en/)
+
+## Configuration and Running
+
+1. Get the project's dependecies, go to your project directory **PokemonApiTest/** and type :
+```
+$ node install
+```
+2. To run all tests of directory **test/**  
+```
+$ npm run all-tests
+```
+3. To run schema test :
+```
+$ npm run schema-test
+```
+4. To run all gets tests :
+ ```
+ $ npm run gets-tests
+ ```
+
+## Documentation
+
+- [Mocha Documentation](https://mochajs.org/)
+- [Chai Documentation](http://chaijs.com/)
+- [Node Documentation](https://nodejs.org/en/docs/)
--- test/pokemon-schema-test.js
+++ test/pokemon-schema-test.js
@@ -5,12 +5,10 @@ let pokemonSchema = require('../schemas/pokemon-schema.js');
 chai.use(require('chai-http'));
 chai.use(require('chai-json-schema'));
 
-describe('Contract Pokemon Test.', function() {
-  it('Validate pokemon contract.', function(done) {
-    chai.request('https://pokeapi.co/api/v2/').get('pokemon/charmander').end(function(err, resp) {
-      expect(resp).to.have.status(200);
-      expect(resp.body).to.be.jsonSchema(pokemonSchema);
-      done();
-    });
+describe('Contract Pokemon Test.',  function() {
+  it('Validate pokemon contract.', async function() {
+    const resp = await chai.request('https://pokeapi.co/api/v2/').get('pokemon/charmander');
+    expect(resp).to.have.status(200);
+    expect(resp.body).to.be.jsonSchema(pokemonSchema);
   });
 });
